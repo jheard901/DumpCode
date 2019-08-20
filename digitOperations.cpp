404b072d89bdfaeb33a43e81203eb040240905c1
jheard901 <heard_jth@yahoo.com>
2019-08-19 23:33:01 -0400

    Operations for addition, subtraction, and multiplication from specified digit completed

--- digitOperations.cpp
+++ digitOperations.cpp
@@ -6,6 +6,8 @@
 
 
 int addNumsAtDigit(int, int, int);
+int subtractNumsAtDigit(int, int, int);
+int multiplyNumsAtDigit(int, int, int);
 int highestDigitInCommon(int, int);
 
 
@@ -46,9 +48,39 @@ int main()
 			break;
 		//Subtraction
 		case 1:
+			std::cout << "\n\nEnter the first number.\n";
+			firstNum = getUserNumber();
+			std::cout << "\n\nEnter the second number.\n";
+			secondNum = getUserNumber();
+			std::cout << "\n\nWhich digit would you like to use for carrying out the operation?.\n";
+			std::cout << "\nBe mindful both numbers must have that same digit placement in common";
+			std::cout << "\ne.g. using 3753 and 16212 the valid digits are 1-4 since the highest digit both have in common is 4.";
+			std::cout << "\nUsing 4 would calculate as 3000 - 16000 in this example.";
+			std::cout << std::endl;
+			digitToUse = getUserNumber(1, highestDigitInCommon(firstNum, secondNum));
+			result = subtractNumsAtDigit(firstNum, secondNum, digitToUse);
+			std::cout << "\nThe difference of " << firstNum << " and " << secondNum << " at the " << digitToUse << " digit of each number is " << result;
+
+			std::cout << "\n\nPress enter to continue.\n";
+			getwchar();
 			break;
 		//Multiplication
 		case 2:
+			std::cout << "\n\nEnter the first number.\n";
+			firstNum = getUserNumber();
+			std::cout << "\n\nEnter the second number.\n";
+			secondNum = getUserNumber();
+			std::cout << "\n\nWhich digit would you like to use for carrying out the operation?.\n";
+			std::cout << "\nBe mindful both numbers must have that same digit placement in common";
+			std::cout << "\ne.g. using 4375 and 922 the valid digits are 1-3 since the highest digit both have in common is 3.";
+			std::cout << "\nUsing 2 would calculate as 4370 * 920 in this example.";
+			std::cout << std::endl;
+			digitToUse = getUserNumber(1, highestDigitInCommon(firstNum, secondNum));
+			result = multiplyNumsAtDigit(firstNum, secondNum, digitToUse);
+			std::cout << "\nThe resulting product of " << firstNum << " and " << secondNum << " at the " << digitToUse << " digit of each number is " << result;
+
+			std::cout << "\n\nPress enter to continue.\n";
+			getwchar();
 			break;
 		//Exit Program
 		case 3:
@@ -105,6 +137,91 @@ int addNumsAtDigit(int first, int second, int digit)
 	{
 		//this just means there is an error if digit is less than 1
 		return -9999;
+	}
+}
+
+
+int subtractNumsAtDigit(int first, int second, int digit)
+{
+	if (digit == 1)
+	{
+		return first - second;
+	}
+	else if (digit > 1)
+	{
+		int firstLength = std::to_string(first).length();
+		int secondLength = std::to_string(second).length();
+		int firstValue = 0;
+		int secondValue = 0;
+		int base = toPower(10, digit - 1);
+		std::string strFirst;
+		std::string strSecond;
+
+		//stores the numbers from 'digit' extending to the leftmost char of string
+		for (int i = 0; i <= firstLength - digit; i++)
+		{
+			std::string sTemp = std::to_string(first);
+			int nTemp = sTemp[firstLength - digit - i] - '0';
+			strFirst.insert(0, std::to_string(nTemp));
+		}
+		for (int i = 0; i <= secondLength - digit; i++)
+		{
+			std::string sTemp = std::to_string(second);
+			int nTemp = sTemp[secondLength - digit - i] - '0';
+			strSecond.insert(0, std::to_string(nTemp));
+		}
+
+		firstValue = std::stoi(strFirst);
+		secondValue = std::stoi(strSecond);
+
+		return (firstValue * base) - (secondValue * base);
+	}
+	else
+	{
+		//this just means there is an error if digit is less than 1
+		return -8999;
+	}
+}
+
+int multiplyNumsAtDigit(int first, int second, int digit)
+{
+	if (digit == 1)
+	{
+		return first * second;
+	}
+	else if (digit > 1)
+	{
+		int firstLength = std::to_string(first).length();
+		int secondLength = std::to_string(second).length();
+		int firstValue = 0;
+		int secondValue = 0;
+		int base = toPower(10, digit - 1);
+		std::string strFirst;
+		std::string strSecond;
+
+		//stores the numbers from 'digit' extending to the leftmost char of string
+		for (int i = 0; i <= firstLength - digit; i++)
+		{
+			std::string sTemp = std::to_string(first);
+			int nTemp = sTemp[firstLength - digit - i] - '0';
+			strFirst.insert(0, std::to_string(nTemp));
+		}
+		for (int i = 0; i <= secondLength - digit; i++)
+		{
+			std::string sTemp = std::to_string(second);
+			int nTemp = sTemp[secondLength - digit - i] - '0';
+			strSecond.insert(0, std::to_string(nTemp));
+		}
+
+		firstValue = std::stoi(strFirst);
+		secondValue = std::stoi(strSecond);
+
+		return (firstValue * base) * (secondValue * base);
+	}
+	else
+	{
+		//this just means there is an error if digit is less than 1
+		return -7999;
 	}
 }
 
